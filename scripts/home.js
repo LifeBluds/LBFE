93004290ffa3da8d344e65f1a12d4f8e4dbc3e34
oxayomide <olaoluwastephen32@gmail.com>
2024-06-14 17:51:06 +0100

    home redesign

---
+++ scripts/home.js
@@ -0,0 +1,44 @@
+let selectedCard = null;
+
+document.getElementById('donorCard').addEventListener('click', function() {
+    document.getElementById('continueBtn').disabled = false;
+    document.getElementById('continueBtn').classList.remove('cursor-not-allowed');
+    document.getElementById('continueBtn').classList.add('cursor-pointer');
+    document.getElementById('donorCard').classList.add('selected');
+    document.getElementById('facilityCard').classList.remove('selected');
+    selectedCard = 'donor';
+    document.querySelector('#donorCard i').classList.add('text-dark-red-500');
+    document.querySelector('#facilityCard i').classList.remove('text-dark-red-500');
+});
+
+document.getElementById('facilityCard').addEventListener('click', function() {
+    document.getElementById('continueBtn').disabled = false;
+    document.getElementById('continueBtn').classList.remove('cursor-not-allowed');
+    document.getElementById('continueBtn').classList.add('cursor-pointer');
+    document.getElementById('facilityCard').classList.add('selected');
+    document.getElementById('donorCard').classList.remove('selected');
+    selectedCard = 'facility';
+    document.querySelector('#facilityCard i').classList.add('text-dark-red-500');
+    document.querySelector('#donorCard i').classList.remove('text-dark-red-500');
+});
+
+document.getElementById('continueBtn').addEventListener('click', function() {
+    if (selectedCard === 'donor') {
+        window.location.href = 'donor-register.html';
+    } else if (selectedCard === 'facility') {
+        window.location.href = 'facility-register.html';
+    }
+});
+
+document.getElementById('leftContent').addEventListener('click', function(event) {
+    if (!event.target.closest('#donorCard') && !event.target.closest('#facilityCard') && selectedCard) {
+        document.getElementById('continueBtn').disabled = true;
+        document.getElementById('continueBtn').classList.add('cursor-not-allowed');
+        document.getElementById('continueBtn').classList.remove('cursor-pointer');
+        document.getElementById('donorCard').classList.remove('selected');
+        document.getElementById('facilityCard').classList.remove('selected');
+        selectedCard = null;
+        document.querySelector('#donorCard i').classList.remove('text-dark-red-500');
+        document.querySelector('#facilityCard i').classList.remove('text-dark-red-500');
+    }
+});
